5fcf0e9df11273c7aa5285cd8fa3709631779b8c
ntt123 <ntt123@gmail.com>
2017-11-26 13:12:38 +0800

    GM

--- src/genModel.py
+++ src/genModel.py
@@ -475,10 +475,10 @@ for _ in range(10000000000000000):
         f.close()
 
 
-        system('mv'+ "best-network-bash", "../docs")
-        system('mv '+ myhash + ".buf ", "../docs")
-        system('mv '+ myhash + ".meta.json", "../docs")
-        system('mv '+ myhash + ".proto.json", "../docs")
+        system('mv'+ "best-network-bash ../docs")
+        system('mv '+ myhash + ".buf ../docs")
+        system('mv '+ myhash + ".meta.json ../docs")
+        system('mv '+ myhash + ".proto.json ../docs")
         system("git add ../docs/best-network-hash ../docs/" + myhash + ".*")
         system("git commit -m "+ myhash)
         system("git push origin master")
