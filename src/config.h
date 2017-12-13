5b5b9f8616b9eb957680ddb657d38707e29062cc
TN <xcodevn@gmail.com>
2017-12-13 09:51:50 +0800

    New branch for testing

--- src/config.h
+++ src/config.h
@@ -32,14 +32,14 @@
 #endif
 
 /* Features */
-//#define USE_BLAS
-//#define USE_OPENBLAS
+#define USE_BLAS
+#define USE_OPENBLAS
 //#define USE_MKL
-//#define USE_OPENCL
+#define USE_OPENCL
 //#define USE_TUNER
 #define USE_IPC
 // Remember to turn on USE_BLAS, USE_OPENBLAS and USE_OPENCL when using USE_SERVER_TEST
-//#define USE_IPC_TEST
+#define USE_IPC_TEST
 
 #define PROGRAM_NAME "Leela Zero"
 #define PROGRAM_VERSION "0.9"
