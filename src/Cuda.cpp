830d92c73c35483c742bbdcb67cc961662886184
ntt123 <ntt123@gmail.com>
2017-12-15 12:56:50 +0800

    Test darknet

---
+++ src/Cuda.cpp
@@ -0,0 +1,233 @@
+#include <algorithm>
+#include <functional>
+#include <cassert>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <iterator>
+#include <string>
+#include <memory>
+#include <cmath>
+#include <array>
+#include <thread>
+#include <vector>
+#include <cstdio>
+
+extern "C" {
+#include "darknet/include/darknet.h"
+#include <assert.h>
+#include <math.h>
+#include <unistd.h>
+}
+
+// Input + residual block tower
+static std::vector<std::vector<float>> conv_weights;
+static std::vector<std::vector<float>> conv_biases;
+static std::vector<std::vector<float>> batchnorm_means;
+static std::vector<std::vector<float>> batchnorm_variances;
+
+// Policy head
+static std::vector<float> conv_pol_w;
+static std::vector<float> conv_pol_b;
+static std::array<float, 2> bn_pol_w1;
+static std::array<float, 2> bn_pol_w2;
+
+static std::array<float, 261364> ip_pol_w;
+static std::array<float, 362> ip_pol_b;
+
+// Value head
+static std::vector<float> conv_val_w;
+static std::vector<float> conv_val_b;
+static std::array<float, 1> bn_val_w1;
+static std::array<float, 1> bn_val_w2;
+
+static std::array<float, 92416> ip1_val_w;
+static std::array<float, 256> ip1_val_b;
+
+static std::array<float, 256> ip2_val_w;
+static std::array<float, 1> ip2_val_b;
+
+std::string cfg_weightsfile;
+
+class Cuda_Network {
+public:
+    void initialize();
+    // void ensure_thread_initialized(void);
+    // std::string get_device_name();
+    // void forward(const std::vector<float>& input, std::vector<float>& output);
+    network * darknet;
+
+private:
+    // nothing
+};
+
+
+void Cuda_Network::initialize() {
+    std::string cfg = std::string();
+    cfg.append(R"(
+[net]
+batch=1
+subdivisions=1
+height=19
+width=19
+channels=18
+momentum=0.9
+decay=0.0005
+
+[convolutional]
+filters=18
+size=3
+stride=1
+pad=1
+activation=relu
+batch_normalize=1
+
+[convolutional]
+filters=64
+size=3
+stride=1
+pad=1
+activation=relu
+batch_normalize=1
+
+[softmax]
+)");
+
+
+    char * cfg_file_name = std::tmpnam(nullptr);
+
+    std::ofstream out(cfg_file_name);
+    out << cfg;
+    out.close();
+
+    darknet = parse_network_cfg(cfg_file_name);
+
+    // Count size of the network
+    printf("Detecting residual layers...");
+    std::ifstream wtfile(cfg_weightsfile);
+    if (wtfile.fail()) {
+        printf("Could not open weights file: %s\n", cfg_weightsfile.c_str());
+        exit(EXIT_FAILURE);
+    }
+    std::string line;
+    auto linecount = size_t{0};
+    auto format_version = -1;
+    while (std::getline(wtfile, line)) {
+        std::stringstream iss(line);
+        // First line is the file format version id
+        if (linecount == 0) {
+           iss >> format_version;
+           printf("v%d...", format_version);
+        }
+        // Third line of parameters are the convolution layer biases,
+        // so this tells us the amount of channels in the residual layers.
+        // (Provided they're all equally large - that's not actually required!)
+        if (linecount == 2) {
+            auto count = std::distance(std::istream_iterator<std::string>(iss),
+                                       std::istream_iterator<std::string>());
+            printf("%d channels...", count);
+        }
+        linecount++;
+    }
+    // 1 format id, 1 input layer (4 x weights), 14 ending weights,
+    // the rest are residuals, every residual has 8 x weight lines
+    auto residual_blocks = linecount - (1 + 4 + 14);
+    if (residual_blocks % 8 != 0) {
+        printf("\nInconsistent number of weights in the file.\n");
+        exit(EXIT_FAILURE);
+    }
+    residual_blocks /= 8;
+    printf("%d blocks\n", residual_blocks);
+    // Re-read file and process
+    wtfile.clear();
+    wtfile.seekg(0, std::ios::beg);
+
+    // Get the file format id out of the way
+    std::getline(wtfile, line);
+
+    auto plain_conv_layers = 1 + (residual_blocks * 2);
+    auto plain_conv_wts = plain_conv_layers * 4;
+    linecount = 0;
+    while (std::getline(wtfile, line)) {
+        std::vector<float> weights;
+        float weight;
+        std::istringstream iss(line);
+        while (iss >> weight) {
+            weights.emplace_back(weight);
+        }
+        if (linecount < plain_conv_wts) {
+            if (linecount % 4 == 0) {
+                conv_weights.emplace_back(weights);
+            } else if (linecount % 4 == 1) {
+                conv_biases.emplace_back(weights);
+            } else if (linecount % 4 == 2) {
+                batchnorm_means.emplace_back(weights);
+            } else if (linecount % 4 == 3) {
+                batchnorm_variances.emplace_back(weights);
+            }
+        } else if (linecount == plain_conv_wts) {
+            conv_pol_w = std::move(weights);
+        } else if (linecount == plain_conv_wts + 1) {
+            conv_pol_b = std::move(weights);
+        } else if (linecount == plain_conv_wts + 2) {
+            std::copy(begin(weights), end(weights), begin(bn_pol_w1));
+        } else if (linecount == plain_conv_wts + 3) {
+            std::copy(begin(weights), end(weights), begin(bn_pol_w2));
+        } else if (linecount == plain_conv_wts + 4) {
+            std::copy(begin(weights), end(weights), begin(ip_pol_w));
+        } else if (linecount == plain_conv_wts + 5) {
+            std::copy(begin(weights), end(weights), begin(ip_pol_b));
+        } else if (linecount == plain_conv_wts + 6) {
+            conv_val_w = std::move(weights);
+        } else if (linecount == plain_conv_wts + 7) {
+            conv_val_b = std::move(weights);
+        } else if (linecount == plain_conv_wts + 8) {
+            std::copy(begin(weights), end(weights), begin(bn_val_w1));
+        } else if (linecount == plain_conv_wts + 9) {
+            std::copy(begin(weights), end(weights), begin(bn_val_w2));
+        } else if (linecount == plain_conv_wts + 10) {
+            std::copy(begin(weights), end(weights), begin(ip1_val_w));
+        } else if (linecount == plain_conv_wts + 11) {
+            std::copy(begin(weights), end(weights), begin(ip1_val_b));
+        } else if (linecount == plain_conv_wts + 12) {
+            std::copy(begin(weights), end(weights), begin(ip2_val_w));
+        } else if (linecount == plain_conv_wts + 13) {
+            std::copy(begin(weights), end(weights), begin(ip2_val_b));
+        }
+        linecount++;
+    }
+    wtfile.close();
+#ifdef USE_OPENCL
+    // input
+    size_t weight_index = 0;
+    opencl_net.push_convolve(3, conv_weights[weight_index],
+                                conv_biases[weight_index]);
+    opencl_net.push_batchnorm(361, batchnorm_means[weight_index],
+                                   batchnorm_variances[weight_index]);
+    weight_index++;
+
+    // residual blocks
+    for (auto i = size_t{0}; i < residual_blocks; i++) {
+        opencl_net.push_residual(3, conv_weights[weight_index],
+                                    conv_biases[weight_index],
+                                    batchnorm_means[weight_index],
+                                    batchnorm_variances[weight_index],
+                                    conv_weights[weight_index + 1],
+                                    conv_biases[weight_index + 1],
+                                    batchnorm_means[weight_index + 1],
+                                    batchnorm_variances[weight_index + 1]);
+        weight_index += 2;
+    }
+    printf("done\n");
+#endif
+}
+
+int main() {
+    cfg_weightsfile = std::string("251b7ca85b3fae0e19e55e9f79abdc5ec3d6832ad2819131581127a0c0c6b4b0");
+    Cuda_Network net;
+
+    net.initialize();
+
+    return 0;
+}
+
